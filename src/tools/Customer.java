f8af38d78e6876719474faf23a5153e0e1367587
wanhui <yao.wanhui@gmail.com>
2015-07-19 20:42:07 -0400

    new file:   src/tools/Customer.java 	deleted:    src/tools/CustomerManager.java

---
+++ src/tools/Customer.java
@@ -0,0 +1,102 @@
+package tools;
+import org.dom4j.Element;
+import org.dom4j.tree.DefaultElement;
+/**
+ * @author comp6231.team5
+ *
+ */
+public class Customer{
+	public int customerReferenceNumber;
+	public String name;
+	public String password;
+	public String street1;
+	public String street2;
+	public String city;
+	public String state;
+	public String zip;
+	public String country;
+
+	/**
+	 * Constructor
+	 * @param int customerReferenceNumber
+	 * @param String name
+	 * @param String password
+	 * @param String street1
+	 * @param String street2
+	 * @param String city
+	 * @param String state
+	 * @param String zip
+	 * @param String country
+	 */
+	public Customer(int customerReferenceNumber,
+			String name,
+			String password,
+			String street1,
+			String street2,
+			String city,
+			String state,
+			String zip,
+			String country){
+		this.customerReferenceNumber = customerReferenceNumber;
+		this.name = name;
+		this.password = password;
+		this.street1 = street1;
+		this.street2 = street2;
+		this.city = city;
+		this.state = state;
+		this.zip = zip;
+		this.country = country;
+	}
+	
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	public String toString(){
+		return "CustomerReferenceNumber:" + customerReferenceNumber 
+				+ ", Name:" + name
+				+ ", Password:" + password
+				+ ", Street1:" + street1
+				+ ", Street2:" + street2
+				+ ", City:" + city
+				+ ", State:" + state
+				+ ", Zip:" + zip
+				+ ", Country:" + country;
+	}
+	
+	
+	/**
+	 * store the customer to an xml element
+	 * @return element format of the customer
+	 */
+	public Element toXmlElement() {
+		DefaultElement customerElem = new DefaultElement("customer");
+		
+		Element subElem = customerElem.addElement("customerReferenceNumber");
+		subElem.setText(String.valueOf(customerReferenceNumber));
+		
+		subElem = customerElem.addElement("name");
+		subElem.setText(name);
+		
+		subElem = customerElem.addElement("password");
+		subElem.setText(password);
+		
+		subElem = customerElem.addElement("street1");
+		subElem.setText(street1);
+		
+		subElem = customerElem.addElement("street2");
+		subElem.setText(street2);
+		
+		subElem = customerElem.addElement("city");
+		subElem.setText(city);
+		
+		subElem = customerElem.addElement("state");
+		subElem.setText(state);
+		
+		subElem = customerElem.addElement("zip");
+		subElem.setText(zip);
+		
+		subElem = customerElem.addElement("country");
+		subElem.setText(country);
+		return customerElem;
+	}
+}
